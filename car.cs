16fc1436cf6757d4dbfd9c8dfaf822a2d4928b67
Javier Villanueva <122271474+jvillanueva-iti@users.noreply.github.com>
2023-04-04 14:46:26 +0200

    Update car.cs

--- car.cs
+++ car.cs
@@ -4,6 +4,17 @@ class Car
 
   static void Main(string[] args)
   {
+    var a = double.NaN;
+
+if (a == double.NaN) // Noncompliant; always false
+{
+  Console.WriteLine("a is not a number");  // this is dead code
+}
+if (a != double.NaN)  // Noncompliant; always true
+{
+  Console.WriteLine("a is not NaN"); // this statement is not necessarily true
+}
+    
     Car myObj = new Car();
     Console.WriteLine(myObj.color);
   }
