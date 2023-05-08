8f5b46e122cc5a68677133b3331fa1b6a2a9841a
Javier Villanueva <122271474+jvillanueva-iti@users.noreply.github.com>
2023-05-08 11:03:38 +0200

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,4 +1,9 @@
 # This program adds two numbers
+target = -5
+num = 3
+
+target =- num  # Noncompliant; target = -3. Is that really what's meant?
+target =+ num # Noncompliant; target = 3
 
 # Modificado
 
